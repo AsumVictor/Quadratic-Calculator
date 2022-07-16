fa48c970d6bce1af3293413a253df72de17fb307
Asumvictor <iamasum369@gmail.com>
2022-07-16 10:38:21 -0700

    Testing real root numetator

--- app.js
+++ app.js
@@ -6,13 +6,21 @@ function a(a,b,c) {
     let absolutedescriminantValue = Math.abs(descriminantValue);
      numeratorpositive = (-1*b) + descriminantValue;
      squareroot_absolutedescriminantValue = Math.sqrt(absolutedescriminantValue)
+     denominaor = (2*a)
     console.log("Discriminant: ",descriminantValue);
     console.log("Discriminant Positive: ",absolutedescriminantValue);
+
+    // for real roots
      if (descriminantValue>=0) {
         console.log("Squre root Discriminant: ",squareroot_absolutedescriminantValue);
+        positivenumerator = (-1*b) + absolutedescriminantValue;
+        negativenumerator = (-1*b) - absolutedescriminantValue;
+        console.log(positivenumerator)
+        console.log(negativenumerator)
      } else {
         console.log("Squre root Discriminant: ",squareroot_absolutedescriminantValue+"i");
      }
 }
 
-a(1,-7,3);
+a(1,0,9);
+a(3,5,2);
