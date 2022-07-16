8957a080fc94478e66a40da706e06c78646fbc07
Asumvictor <iamasum369@gmail.com>
2022-07-16 10:15:50 -0700

    Added Descriminant

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 //let a = document.getElementById("a").value;
 //let b = document.getElementById("b").value;
 // let c = document.getElementById("c").value;
-
-function descriminant(a,b,c) {
+function a(a,b,c) {
     let descriminantValue = (b*b)-(4*a*c);
-    let absolutedescriminantValue = AbstractRange(descriminantValue);
-    //let numeratorpositive = (-1*b) + descriminantValue;
-    console.log(descriminantValue);
-    console.log(absolutedescriminantValue);
+    let absolutedescriminantValue = Math.abs(descriminantValue);
+     numeratorpositive = (-1*b) + descriminantValue;
+     squareroot_absolutedescriminantValue = Math.sqrt(absolutedescriminantValue)
+    console.log("Discriminant: ",descriminantValue);
+    console.log("Discriminant Positive: ",absolutedescriminantValue);
+     if (descriminantValue>=0) {
+        console.log("Squre root Discriminant: ",squareroot_absolutedescriminantValue);
+     } else {
+        console.log("Squre root Discriminant: ",squareroot_absolutedescriminantValue+"i");
+     }
 }
 
-descriminant(6,-7,3);
- 
+a(1,-7,3);
