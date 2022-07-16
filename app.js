81e58ca39ac5f3b2ff31bf2b4b01ba0639ba6682
Asumvictor <iamasum369@gmail.com>
2022-07-15 22:32:18 -0700

    Updated

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
-let a = parseInt(document.getElementById("a")).value;
-let b = parseInt(document.getElementById("b")).value;
-let c = parseInt(document.getElementById("c")).value;
+//let a = document.getElementById("a").value;
+//let b = document.getElementById("b").value;
+// let c = document.getElementById("c").value;
 
-console.log(a)
+function descriminant(a,b,c) {
+    let descriminantValue = (b*b)-(4*a*c);
+    let absolutedescriminantValue = AbstractRange(descriminantValue);
+    //let numeratorpositive = (-1*b) + descriminantValue;
+    console.log(descriminantValue);
+    console.log(absolutedescriminantValue);
+}
+
+descriminant(6,-7,3);
+ 
