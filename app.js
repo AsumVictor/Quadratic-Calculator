3592ad5c8d90474c36020327b087ec37c3cef14e
Asumvictor <iamasum369@gmail.com>
2022-07-17 00:54:12 -0700

    Updated with few designs

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const btn = document.getElementById("calculate");
-
+const answer1 = document.getElementById("root1");
+const answer2 = document.getElementById("root2");
 const  answerDiv = document.querySelector('.root');
+
 function calculate() {
     let a = document.getElementById("a").value;
    let  b = document.getElementById("b").value;
@@ -18,17 +20,19 @@ function calculate() {
         negative_numerator = (-1*b) - absolutedescriminantValue;
             positive_answer = positive_numerator/denominator
             negative_answer = negative_numerator/denominator
-            root1 = positive_numerator.toFixed(3);
-            root2 = negative_answer.toFixed(3);
-        answerDiv.textContent = "The Roots are x =" + root1 + " or X =" + root2;
+            root1 = positive_numerator.toFixed(2);
+            root2 = negative_answer.toFixed(2);
      } else {
         first_Answer = (-1*b)/denominator;
-        positive_second_term = (squareroot_absolutedescriminantValue/denominator)+"i";
-        negative_second_term = ((-1*squareroot_absolutedescriminantValue)/denominator)+"i";
-        root1 = first_Answer + " + " + positive_second_term;
-        root2 = first_Answer  +  negative_second_term;
-        answerDiv.textContent = "The Roots are x = " + root1 + " OR " + root2;
+        positive_second_term = (squareroot_absolutedescriminantValue/denominator).toFixed(2)+"i";
+        negative_second_term = ((-1*squareroot_absolutedescriminantValue)/denominator).toFixed(2)+"i";
+        root1 = first_Answer.toFixed(2) + " + " + positive_second_term;
+        root2 = first_Answer.toFixed(2)  + " " +  negative_second_term;
      }
+
+     answerDiv.classList.add("show");
+     answer1.textContent = root1;
+     answer2.textContent = root2;
 }
 
 btn.addEventListener("click",calculate);
